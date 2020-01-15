5728da2e16186351e7480259fe174b76565d2945
Taylor Mills <taylormills24@yahoo.com>
2020-01-15 10:38:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,14 @@
-class Person 
-   def initialize (name)
-     @name = name
-   end
-
-  def name=(name)
-     @name = name
-   end
-
-def name
-  @name
+class Person
+  def initialize (persons_name)
+    @name = persons_name
   end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+  
 end
-
-
